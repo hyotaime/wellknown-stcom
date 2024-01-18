a3e4bbf7c0f4349df787ab13473f98b2dd56448f
hyotaime <hyotaime@hyotaime.com>
2024-01-18 21:18:49 +0900

    feat: Divide library and outer link section

--- src/main/java/com/hirame59/wellknownstcom/domain/Book.java
+++ src/main/java/com/hirame59/wellknownstcom/domain/Book.java
@@ -19,6 +19,8 @@ public class Book {
     private String name;
     private String author;
     private String etc;
+    private String liblinkinfo;
+    private String liblink;
     private String linkinfo;
     private String link;
 }
